b0adb81916616e0cee2682b121b936be8abcfc7b
KennedyOkoth <keinokoth.ok@gmail.com>
2023-10-15 19:13:59 +0300

    Completed tests

---
+++ tests/test_console.py
@@ -0,0 +1,201 @@
+#!/usr/bin/python3
+"""This test module defines tests for console.py"""
+
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from console import HBNBCommand
+
+
+class TestHBNBCommand(unittest.TestCase):
+    """
+    Unit tests for the HBNBCommand class
+    """
+
+    def tearDown(self):
+        """
+        Clean up the test environment after each test method is executed.
+        """
+        self.console = None
+
+    def test_quit_command(self):
+        """
+        Test the quit command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+            self.assertEqual(f.getvalue(), "")
+
+    def test_EOF_command(self):
+        """
+        Test the EOF command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
+            self.assertEqual(f.getvalue(), "\n")
+
+    def test_emptyline_command(self):
+        """
+        Test the emptyline command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.assertIsNone(HBNBCommand().onecmd(""))
+            self.assertEqual(f.getvalue(), "")
+
+    def test_create_command(self):
+        """
+        Test the create command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create BaseModel")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) > 0)
+
+    def test_show_command(self):
+        """
+        Test the show command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_destroy_command(self):
+        """
+        Test the destroy command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy BaseModel 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_all_command(self):
+        """
+        Test the all command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("all")
+            output = f.getvalue().strip()
+            self.assertTrue(len(output) > 0)
+
+    def test_update_command(self):
+        """
+        Test the update command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd('update BaseModel 1234-5678-9012 name "John Doe"')
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_help(self):
+        """
+        Test the help command.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("help")
+            output = f.getvalue().strip()
+            self.assertIn("Documented commands", output)
+            self.assertIn("EOF  all  create  destroy  help  quit  show  update", output)
+
+    def test_create_missing_class_name(self):
+        """
+        Test the create command with missing class name.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("create")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_show_missing_class_name(self):
+        """
+        Test the show command with missing class name.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("show")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_destroy_missing_class_name(self):
+        """
+        Test the destroy command with missing class name.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_update_missing_class_name(self):
+        """
+        Test the update command with missing class name.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("update")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** class name missing **")
+
+    def test_show_missing_instance_id(self):
+        """
+        Test the show command with missing instance id.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_destroy_missing_instance_id(self):
+        """
+        Test the destroy command with missing instance id.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy BaseModel")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_update_missing_instance_id(self):
+        """
+        Test the update command with missing instance id.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("update BaseModel")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** instance id missing **")
+
+    def test_user_show_command(self):
+        """
+        Test the show command for User class.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("show User 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_user_destroy_command(self):
+        """
+        Test the destroy command for User class.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy User 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_base_model_show_command(self):
+        """
+        Test the show command for BaseModel class.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("show BaseModel 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+    def test_base_model_destroy_command(self):
+        """
+        Test the destroy command for BaseModel class.
+        """
+        with patch("sys.stdout", new=StringIO()) as f:
+            HBNBCommand().onecmd("destroy BaseModel 1234-5678-9012")
+            output = f.getvalue().strip()
+            self.assertEqual(output, "** no instance found **")
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_user.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""This test module defines tests for user.py"""
+
+import unittest
+from models.user import User
+from models import storage
+
+
+class TestUser(unittest.TestCase):
+    """test user class"""
+
+    def setUp(self):
+        """set up User class"""
+        self.user = User()
+
+    def test_initialize(self):
+        """initialize User class"""
+        self.assertEqual(self.user.email, "")
+        self.assertEqual(self.user.password, "")
+        self.assertEqual(self.user.first_name, "")
+        self.assertEqual(self.user.last_name, "")
+
+    def test_user_attributes(self):
+        """test attribute setup"""
+        user = User()
+        self.assertEqual(user.email, "")
+        self.assertEqual(user.password, "")
+        self.assertEqual(user.first_name, "")
+        self.assertEqual(user.last_name, "")
+
+    def test_user_creation(self):
+        """set values for attributes"""
+        user = User(
+            email="test@example.com",
+            password="password",
+            first_name="John",
+            last_name="Doe",
+        )
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.password, "password")
+        self.assertEqual(user.first_name, "John")
+        self.assertEqual(user.last_name, "Doe")
+
+    def test_user_to_dict(self):
+        """set dictionary for user"""
+        user = User(
+            email="test@example.com",
+            password="password",
+            first_name="John",
+            last_name="Doe",
+        )
+        user_dict = user.to_dict()
+        self.assertEqual(user_dict["__class__"], "User")
+        self.assertEqual(user_dict["email"], "test@example.com")
+        self.assertEqual(user_dict["password"], "password")
+        self.assertEqual(user_dict["first_name"], "John")
+        self.assertEqual(user_dict["last_name"], "Doe")
+
+    def test_user_from_dict(self):
+        """collect data from dict"""
+        user_data = {
+            "__class__": "User",
+            "id": "123",
+            "email": "test@example.com",
+            "password": "password",
+            "first_name": "John",
+            "last_name": "Doe",
+        }
+        user = User(**user_data)
+        self.assertEqual(user.id, "123")
+        self.assertEqual(user.email, "test@example.com")
+        self.assertEqual(user.password, "password")
+        self.assertEqual(user.first_name, "John")
+        self.assertEqual(user.last_name, "Doe")
+
+
+if __name__ == "__main__":
+    unittest.main()
